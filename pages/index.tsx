dc039b747d94bd541e7bf985a422181c0e03ae94
Alejandro Cremades <ale@alecrem.com>
2022-08-28 09:43:04 +0900

    Simplest connect/disconnect component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,17 @@
 import type { NextPage } from 'next'
-import { Container, Heading } from '@chakra-ui/react'
+import { Container, Heading, Box } from '@chakra-ui/react'
+import { useAccount } from 'wagmi'
+import { Connect } from '../components'
+import { useMounted } from '../hooks'
 
 const Home: NextPage = () => {
+  const isMounted = useMounted()
+  const { isConnected } = useAccount()
+
   return (
     <Container maxW="container.lg" mt="2em">
       <Heading>ヘンカク カルタ</Heading>
+      <Box mt="2em">{isMounted && <Connect />}</Box>
     </Container>
   )
 }
