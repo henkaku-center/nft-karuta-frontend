46c87b3e29ad7ad8ac72cfefcba5702647c90571
yuwa2020 <yudaihirata411@gmail.com>
2022-09-18 20:13:21 +0900

    Fetch metadata, show image and properties

--- components/TokenDisplay.tsx
+++ components/TokenDisplay.tsx
@@ -1,8 +1,12 @@
+import { useState, useEffect } from 'react'
 import { useNetwork } from 'wagmi'
-import { Badge, ListItem, Link } from '@chakra-ui/react'
-import { CheckIcon } from '@chakra-ui/icons'
+import { Badge, Box, Link, Image, Heading, Text } from '@chakra-ui/react'
+import { CheckIcon, NotAllowedIcon } from '@chakra-ui/icons'
 import { useTokenURI } from '../hooks/'
-import { getContractAddress } from '../utils/contractAddress'
+import {
+  getContractAddress,
+  openSeaTokenAddress
+} from '../utils/contractAddress'
 import { tokenIdWithKana } from '../utils/tokenIds'
 
 export function TokenDisplay({ token }: { token: tokenIdWithKana }) {
@@ -15,15 +19,57 @@ export function TokenDisplay({ token }: { token: tokenIdWithKana }) {
     erc1155Contract,
     token.tokenId || '0'
   )
+  const [tokenMetadata, setTokenMetadata] = useState({})
+  useEffect(() => {
+    if (processedTokenURI.length === 0) return
+
+    //IPFS not supported yet
+    if (processedTokenURI.indexOf('ipfs://') === 0) return
+
+    const fetchTokenMetadata = async () => {
+      try {
+        const response = await fetch(processedTokenURI)
+        if (response.ok !== true) {
+          throw 'Error fetching metadata: ' + response.status
+        }
+        const newData = await response.json()
+        setTokenMetadata(newData)
+      } catch (error) {
+        console.log('error: ', error)
+      }
+    }
+    fetchTokenMetadata()
+  }, [processedTokenURI])
+  console.log(tokenMetadata.traits)
   return (
     <>
-      <ListItem key={token.tokenId}>
-        <Link href={processedTokenURI}>
-          <Badge colorScheme="green" mr="0.5em">
-            <CheckIcon></CheckIcon> {token.kana}
-          </Badge>
-        </Link>
-      </ListItem>
+      <Box w="176px" h="200px">
+        {JSON.stringify(tokenMetadata) !== '{}' ? (
+          <>
+            <Image src={tokenMetadata.image} alt={tokenMetadata.name} />
+            <Link href={openSeaTokenAddress + token.tokenId}>
+              <Heading as="h3" size="sm" mt="1em">
+                {tokenMetadata.name}
+              </Heading>
+            </Link>
+            <Text mt="1em">Available properties:</Text>
+            {tokenMetadata.traits.map(({ value }) => (
+              <Badge colorScheme="green" mr="4px">
+                <CheckIcon></CheckIcon>
+                {value}
+              </Badge>
+            )
+            )}
+          </>
+        ) : (
+          <>
+            <Box w="176px" h="176px"></Box>
+            <Link href={processedTokenURI}>
+              <Badge colorScheme="red">Could not fetch: {token.name}</Badge>
+            </Link>
+          </>
+        )}
+      </Box>
     </>
   )
 }
